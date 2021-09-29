0f57d26d344eb384500f53666f62e497bb8104aa
Aashish KC <kcaashish@gmail.com>
2021-09-29 16:01:50 +0545

    added comments

--- email-maker.py
+++ email-maker.py
@@ -2,23 +2,31 @@ import json
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 
+
 def email_maker(path):
     data = pd.read_csv(path, sep=",")
 
+    # makes json file from csv
     with open("data.json", "w") as f:
         f.write(data.to_json(orient='records'))
 
+    # loads json file
     with open("data.json", "r") as f:
         data = json.load(f)
 
+    # give the name of the folder where the template files are located
     file_loader = FileSystemLoader("templates")
     env = Environment(loader=file_loader)
 
+    # select the template file
     rendered = env.get_template("e-template.html").render(data=data)
 
+    # write the rendered template to a file
     f_name = "rendered_email.html"
     with open(f"./site/{f_name}", "w") as f:
         f.write(rendered)
 
+
 if __name__ == "__main__":
+    # add the path to the CSV file as argument
     email_maker('data/09172021_complete_data.csv')
